a03c154f89f9314d9d667f45db173888810fcd0b
Mark van Cuijk <mark@hexon.nl>
2024-03-26 11:11:14 +0100

    Add go.mod and update package path

--- example/test.go
+++ example/test.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/elliotchance/phpserialize"
+
+	"github.com/hexon/phpserialize"
 )
 
 func main() {
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hexon/phpserialize
+
+go 1.21.4
